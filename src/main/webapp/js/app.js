b32c2f03d1608b85adb3e86eddfc8b010edd7172
Heiko Dreyer <mail@boxedfolder.com>
2014-08-12 16:24:00 +0200

    Worked on fe

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,10 +1,20 @@
-var app = angular.module("shubidu", []);
+$(window).bind("load", function() {
+    $(".spinner").hide();
+});
 
+var app = angular.module("shubidu", []);
 app.controller("URLController", function($scope, $http, $location) {
-    $scope.createURL = function createURL() {
+    $scope.createURL = function createURL(form) {
+        if (!form.$valid) {
+            return;
+        }
+        $(".spinner").toggle(1500);
         var data = { link: $scope.link};
-        $http.post("/", data).success(function(data, status, headers) {
+        $http.post("/", data).success(function(data, status, headers, config) {
             $scope.url = data;
+            $(".spinner").toggle(1500);
+        }).error(function(data, status, headers, config){
+            $(".spinner").toggle(1500);
         });
     };
 });
