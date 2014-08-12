1b86f639f8b60319bf27542bf0f692ed684b2f97
Heiko Dreyer <mail@boxedfolder.com>
2014-08-12 20:51:37 +0200

    Reworked markup and styles

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,20 +1,21 @@
-$(window).bind("load", function() {
-    $(".spinner").hide();
-});
-
 var app = angular.module("shubidu", []);
 app.controller("URLController", function($scope, $http, $location) {
     $scope.createURL = function createURL(form) {
         if (!form.$valid) {
             return;
         }
-        $(".spinner").toggle(1500);
         var data = { link: $scope.link};
+        $scope.spinner = true;
         $http.post("/", data).success(function(data, status, headers, config) {
+            $scope.spinner = false;
             $scope.url = data;
-            $(".spinner").toggle(1500);
         }).error(function(data, status, headers, config){
-            $(".spinner").toggle(1500);
+            $scope.spinner = false;
         });
     };
+
+    $scope.reset = function reset() {
+        $scope.url = null;
+        $scope.link = ""
+    }
 });
