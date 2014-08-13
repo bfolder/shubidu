81a5fc4fec85dc205e8486f18769a092688ac161
Heiko Dreyer <mail@boxedfolder.com>
2014-08-13 17:29:33 +0200

    Added db property file

---
+++ src/main/java/com/boxedfolder/shubidu/config/Mode.java
@@ -0,0 +1,7 @@
+package com.boxedfolder.shubidu.config;
+
+public final class Mode {
+    public static final String PRODUCTION = "production";
+    public static final String DEVELOPMENT = "development";
+    public static final String TESTING = "testing";
+}
--- src/main/java/com/boxedfolder/shubidu/config/jpa/JPAEmbeddedDatabaseConfig.java
+++ src/main/java/com/boxedfolder/shubidu/config/jpa/JPAEmbeddedDatabaseConfig.java
@@ -1,12 +1,15 @@
 package com.boxedfolder.shubidu.config.jpa;
 
+import com.boxedfolder.shubidu.config.Mode;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
 
+@Profile(Mode.DEVELOPMENT)
 @Configuration
 public class JPAEmbeddedDatabaseConfig extends JPACommonConfig {
     @Override
